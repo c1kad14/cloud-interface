a6ada2e82ce10501f8b7b6ad62fc26a71a2ea96b
c1kad14 <iurii.bobrykov@gmail.com>
2018-04-17 09:43:02 -0600

    init commit
    created empty springboot app

---
+++ src/test/java/com/example/cloudinterface/CloudInterfaceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.cloudinterface;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudInterfaceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
